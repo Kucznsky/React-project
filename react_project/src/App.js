dc9a0915c87198a4bb4173e863635e2ff8ca6ee8
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-11-22 00:44:44 +0100

    changes in the interface

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, {useState} from 'react';
+import logo from './logo.png'
 
 function App() {
   return (
@@ -15,8 +16,8 @@ const HomeBar = () => {
   return (
     <div className="div1">
       <div className="div2">
-        <h1 className="companyTitle">[Put cinema name here]</h1>
-        <ul>
+        <img src={logo} alt="logo" />
+        <ul className="mainBar">
           <li>
             <button onClick={() => setActive("FirstPage")}>FirstHome</button>
           </li>
@@ -36,8 +37,8 @@ const HomeBar = () => {
         <div>
           {active === "FirstPage" && <HomePage></HomePage>}
           {active === "SecondPage" && <Repertoire></Repertoire>}
-          {active === "ThirdPage" && <ScreeningRooms></ScreeningRooms>}
-          {active === "FourthPage" && <Movies></Movies>}
+          {active === "ThirdPage" && <ScreeningRoomsList></ScreeningRoomsList>}
+          {active === "FourthPage" && <MovieList></MovieList>}
         </div>
       </div>
     </div>
@@ -52,14 +53,69 @@ const Repertoire = () => {
   return(<h1>Here will be repertoire</h1>);
 }
 
-const Movies = () => {
-  return(<h1>Here will be list of movies</h1>);
+const MovieList = () => {
+    return(  
+    <section className='booklist'>
+      {movie.map((movie) => {
+        return (<Movie key={movie.id} {...movie}></Movie>);
+      })}
+    </section>
+  );
 }
 
-const ScreeningRooms = () => {
-  return(<h1>Here will be list of ScreeningRooms</h1>);
+const ScreeningRoomsList = () => {
+  return(  
+    <section className='booklist'>
+      {room.map((room) => {
+        return (<Room key={room.id} {...room}></Room>);
+      })}
+    </section>
+  );;
+}
+
+const Movie = (props) => 
+{
+  const {title, time} = props;
+  return (
+    <article className='book'>
+      <h1>{title}</h1>
+      <h4>{time}</h4>
+    </article>
+  );
+}
+
+const Room = (props) => 
+{
+  const {number, space} = props;
+  return (
+    <article className='book'>
+      <h1>{number}</h1>
+      <h4>{space}</h4>
+    </article>
+  );
 }
 //=======================================================================================================
+const movie = [
+
+{
+  id: 1,
+  title: 'Diuna',
+  time: '155min',
+},
+];
+
+const room = [
+{
+  id: 1,
+  number: 'room 1',
+  space: '100 chairs',
+},
+{
+  id: 2,
+  number: 'room 2',
+  space: '50 chairs',
+},
+];
 
 //=======================================================================================================
 
