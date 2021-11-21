412313f041b78ab37a567477e57df69d51ea0537
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-11-21 20:13:45 +0100

    _

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,10 +1,54 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import React, {useState} from 'react';
 
 function App() {
   return (
     <div>
+      <HomeBar></HomeBar>
     </div>
   );
 }
+
+//=======================================================================================================
+const HomeBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>Repertoire</button>
+        </li>
+        <li>
+          <button>Screening rooms</button>
+        </li>
+        <li>
+          <button>Movies</button>
+        </li>
+        <li>
+          <button></button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+//=======================================================================================================
+/*const [active, setActive] = useState("FirstCard")
+  return(
+    <div>
+      <div>
+        <button onClick={() => setActive("FirstCard")}>First page</button>
+        <button onClick={() => setActive("SecondCard")}>Second page</button>
+      </div>
+      <div>
+        {active === "FirstCard" && <Something></Something>}
+        {active === "SecondCard" && <SomethingElse></SomethingElse>}
+      </div>
+    </div>
+  );
+*/
 
 export default App;
